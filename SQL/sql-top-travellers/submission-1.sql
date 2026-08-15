541f84f2d92e9f2e1762c4dcc4530c60ab7a4d09
DanialWajid <150847611+DanialWajid@users.noreply.github.com>
2026-08-15 21:54:25 +0500

    Add: sql-top-travellers - submission-1

---
+++ SQL/sql-top-travellers/submission-1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    users.name, 
+    CASE
+        WHEN SUM(rides.distance) IS NULL THEN 0
+        ELSE SUM(rides.distance)
+    END AS travelled_distance
+FROM users 
+LEFT JOIN rides ON users.id = rides.user_id 
+GROUP BY users.id
+ORDER BY travelled_distance DESC, users.name ASC;
